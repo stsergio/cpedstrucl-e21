a2ec4a3351ba6fa95dd2fc7e7628958806b1b0f9
stsergio <ashkimbelle11@yahoo.com>
2017-01-19 08:20:50 +0800

    linkedlist_stack.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -2,103 +2,112 @@
 using namespace std;
 
 
-struct list {
-	int data;
-	struct list *next;
-} *node = NULL, *first = NULL, *last = NULL, *node1 = NULL;
 
-class linkedlist {
-	public: 
-
-		
-		void insert() {
-			list *addNew = new list;
-			cout << "Enter value for the node:" << endl;
-			cin >> addNew->data;
-			if(first == NULL) {
-				addNew->next = NULL;
-				first = addNew;
-				last = addNew;
-				cout << "Linked list Created!" << endl;
-			}
-			else {
-				addNew->next = NULL;
-				last->next = addNew;
-				last = addNew;
-				cout << "Data Inserted in the Linked list!" << endl;
-			}
-		}
-
-		
-		void display() {
-			node = first;
-			cout << "List of data in Linked list" << endl;
-			while(node != NULL) {
-				cout << node->data << endl;
-				node = node->next;
-			}
-		}
-	
-		
-		void del() {
-			int count = 0, number, i;
-			for(node = first; node != NULL; node = node->next)
-				count++;
-			display();
-			node = node1 = first;
-			cout << count << " nodes available here!" << endl;
-			cout << "Enter the node number which you want to delete:" << endl;
-			cin >> number;
-			if(number != 1) {
-				if(number <= count) {
-					for(i = 2; i <= number; i++)
-						node = node->next;
-					for(i = 2; i <= number-1; i++)
-						node1 = node1->next;
-					node1->next = node->next;
-					node->next = NULL;
-					node = NULL;
-				}
-				else
-					cout << "Invalid node number!" << endl;
-			}
-			else {
-				first = node->next;
-				node = NULL;
-			}
-			cout << "Node has been deleted successfully!" << endl;
-		}
-	
+struct node
+{
+    int data;
+    struct node *next;
 };
 
-int main() {
-	int op = 0;
-	linkedlist llist = linkedlist();
-	while(op != 4) {
-		cout << "1. Insert << endl;
-		cout << "2. Delete " << endl;
-		cout << "3. Display " << endl;
-		cout << "4. Exit " << endl;
-		cout << "Enter your choice:" << endl;
-		cin >> op;
-		switch(op) {
-			case 1:
-				llist.insert();
-				break;
-			case 2:
-				llist.del();
-				break;
-			case 3:
-				llist.display();
-				break;
-			case 4:
-				cout << "Bye Bye!" << endl;
-				return 0;
-				break;
-			default:
-				cout << "Invalid choice!" << endl;
-		}
-	}
-	return 0;
+
+class stack
+{
+    struct node *top;
+    public:
+    stack() 
+    {
+        top=NULL;
+    }
+    void push(); 
+    void pop();  
+    void show(); 
+};
+
+void stack::push()
+{
+    int value;
+    struct node *ptr;
+    cout<<endl;
+	cout<<"=========================="<<endl;
+	cout<<"PUSH Operation"<<endl;
+    cout<<endl;
+	cout<<"Enter a number to insert: "<<endl;
+    cin>>value;
+    ptr=new node;
+    ptr->data=value;
+    ptr->next=NULL;
+    if(top!=NULL)
+        ptr->next=top;
+    top=ptr;
+    cout<<endl<<"New item is inserted to the stack"<<endl;
+
 }
 
+
+void stack::pop()
+{
+    struct node *temp;
+    if(top==NULL)
+    {
+        cout<<"The stack is empty"<<endl;
+    }
+    temp=top;
+    top=top->next;
+    cout<<endl;
+	cout<<"=========================="<<endl;
+	cout<<"POP Operation"<<endl;
+	cout<<endl;
+	cout<<"Value is "<<endl<<temp->data;
+    delete temp;
+}
+
+
+void stack::show()
+{
+    struct node *ptr1=top;
+    cout<<"The stack is "<<endl;
+    while(ptr1!=NULL)
+    {
+        cout<<ptr1->data<<"- ";
+        ptr1=ptr1->next;
+    }
+    cout<<"NULL"<<endl<<endl;
+}
+
+
+int main()
+{
+    stack s;
+    int choice;
+    while(1)
+    {
+        cout<<endl;
+        cout<<"=========================="<<endl;
+		cout<<"STACK USING LINKED LIST"<<endl;
+        cout<<"1: PUSH"<<endl;
+		cout<<"2: POP"<<endl;
+		cout<<"3: DISPLAY STACK"<< endl;
+		cout<<"4: EXIT"<<endl<<endl;
+        cout<<"Enter your choice(1-4): "<<endl;
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+                s.push();
+                break;
+            case 2:
+                s.pop();
+                break;
+            case 3:
+                s.show();
+                break;
+            case 4:
+                return 0;
+                break;
+            default:
+                cout<<"Invalid option"<<endl;
+                break;
+        }
+    }
+    return 0;
+}
